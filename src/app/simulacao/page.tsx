d6de402ba91c22d6e4f980b0c87849deed28c389
bcxand <102360933+bcxand@users.noreply.github.com>
2025-10-16 17:27:55 -0300

    Update page.tsx

--- src/app/simulacao/page.tsx
+++ src/app/simulacao/page.tsx
@@ -269,7 +269,7 @@ export default function SimulacaoPage() {
             {/* Logo */}
             <Link href="/" className="flex items-center">
               <img 
-                src="https://i.imgur.com/0vW5vV8.png" 
+                src="https://imgur.com/6JWUdFu.png" 
                 alt="Grupo Nexts Investimentos Imobiliários" 
                 className="h-12 w-auto"
                 onError={(e) => {
@@ -623,7 +623,7 @@ export default function SimulacaoPage() {
             <div>
               <div className="flex items-center mb-6">
                 <img 
-                  src="https://i.imgur.com/0vW5vV8.png" 
+                  src="https://imgur.com/6JWUdFu.png" 
                   alt="Grupo Nexts Investimentos Imobiliários" 
                   className="h-12 w-auto"
                   onError={(e) => {
